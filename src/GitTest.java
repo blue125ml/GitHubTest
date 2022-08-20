f64a12b581fe00d142f586d0dbf3c751ff2d571c
yu <blue125ml@naver.com>
2022-08-20 11:40:37 +0900

    four

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,9 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git second");
-        System.out.println("git second");System.out.println("git second");System.out.println("git second");
-        System.out.println("git second");
-        System.out.println("git second");
+        System.out.println("ddd");
+
 
 
     }
