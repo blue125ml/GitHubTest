f18b13b22e05b00133d3cddd737d845c90a5d12d
yu <blue125ml@naver.com>
2022-08-20 13:01:09 +0900

    wrapper

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,12 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("This is first git hub");
+        System.out.println("Int max :" + Integer.MAX_VALUE);
+        System.out.println("Int min :" + Integer.MIN_VALUE);
+        System.out.println("byte max :" + Byte.MAX_VALUE);
+        System.out.println("byte min :" + Byte.MIN_VALUE);
+        System.out.println("Int size :" + Integer.SIZE);
+        System.out.println("float max :" + Float.SIZE);
+        System.out.println("double max :" + Double.SIZE);
 
 
 
