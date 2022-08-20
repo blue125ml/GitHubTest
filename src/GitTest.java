4497f3154828848ad0801a14546154defc0a900b
yu <blue125ml@naver.com>
2022-08-20 11:17:09 +0900

    second

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,11 +1,10 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Git test");
-        System.out.println("git test2");
-        System.out.println("Git test");
-        System.out.println("git test2");        System.out.println("Git test");
-        System.out.println("git test2");        System.out.println("Git test");
-        System.out.println("git test2");
+        System.out.println("git second");
+        System.out.println("git second");System.out.println("git second");System.out.println("git second");
+        System.out.println("git second");
+        System.out.println("git second");
+
 
     }
 }
