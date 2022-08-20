4b19b93a8130c0ba35acf1baaa35b01e5dc2c518
yu <blue125ml@naver.com>
2022-08-20 11:03:42 +0900

    first

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git test");
+        System.out.println("git test2");
+        System.out.println("Git test");
+        System.out.println("git test2");        System.out.println("Git test");
+        System.out.println("git test2");        System.out.println("Git test");
+        System.out.println("git test2");
+
+    }
+}
